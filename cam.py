e748796bc5b517f2b7f760617adbd4bad647ac70
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-10 21:14:04 -0800

    update cam.py
    updated cam.py to add the redording of the time in secods to the image file name.

--- cam.py
+++ cam.py
@@ -7,6 +7,7 @@ camera.start_preview()
 
 cycle = input("# of photos: ")
 for i in range(cycle):
-        camera.capture('/home/pi/images/image%s.jpg' % i)
+	t = time.time()
+        camera.capture('/home/pi/images/image_%s_.jpg' % (t, i))
         sleep(1)
 camera.stop_preview()
