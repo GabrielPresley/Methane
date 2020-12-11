e4dd0a078a925ea9af3221dee77ae617cec31fb8
Earth13wells <72669457+Earth13wells@users.noreply.github.com>
2020-12-10 21:48:09 -0800

    Public (#13)
    * Update cam.py
    
    made the time print in a much more readable fasion
    
    * Update README.md
    
    missed " "
    
    * Update README.md
    
    Co-authored-by: Dugchugy <61024715+Dugchugy@users.noreply.github.com>

--- cam.py
+++ cam.py
@@ -1,11 +1,11 @@
 from picamera import PiCamera
-from time import sleep, time
+from time import sleep, strftime
 
 camera = PiCamera()
 camera.resolution = (100, 100)
 
 cycle = input("# of photos: ")
 for i in range(cycle):
-	t = time()
+	t = strftime()
         camera.capture('/home/pi/images/image_%s_%s.jpg' % (t, i))
         sleep(1)
