af85e7c4841cdb5bf72e20c20ab823054d0cb05d
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-10 21:42:18 -0800

    Update cam.py
    made the time print in a much more readable fasion

--- cam.py
+++ cam.py
@@ -1,5 +1,5 @@
 from picamera import PiCamera
-from time import sleep, time
+from time import sleep, strftime
 
 camera = PiCamera()
 camera.resolution = (100, 100)
@@ -7,7 +7,7 @@ camera.start_preview()
 
 cycle = input("# of photos: ")
 for i in range(cycle):
-	t = time()
+	t = strftime()
         camera.capture('/home/pi/images/image_%s_%s.jpg' % (t, i))
         sleep(1)
 camera.stop_preview()
