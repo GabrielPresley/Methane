786d102c4576719c0f2eaadf8496666855e65c60
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-10 21:27:37 -0800

    Update cam.py
    more error fixing

--- cam.py
+++ cam.py
@@ -7,7 +7,7 @@ camera.start_preview()
 
 cycle = input("# of photos: ")
 for i in range(cycle):
-	t = time.time()
+	t = time()
         camera.capture('/home/pi/images/image_%s_.jpg' % (t, i))
         sleep(1)
 camera.stop_preview()
