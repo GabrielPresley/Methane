1c938bd439eaa672d4d3ab989f31d6b63eb365fa
Earth13wells <72669457+Earth13wells@users.noreply.github.com>
2020-12-11 19:18:02 -0800

    Update arduino-pi.py

--- arduino-pi.py
+++ arduino-pi.py
@@ -1,12 +1,8 @@
 import serial
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.flush()
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
-            
-# TBH I STOLE THIS CODE
-# https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/
-# Also need to set baud tare in the arduino code.
