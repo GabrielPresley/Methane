5045283ff30e658177bb9640d51775fefd35c089
Earth13wells <72669457+Earth13wells@users.noreply.github.com>
2020-12-11 22:27:26 -0800

    Update sensehat.py

--- sensehat.py
+++ sensehat.py
@@ -10,6 +10,11 @@ camera.resolution = (100, 100)
 sense = SenseHat()
 sense.clear() #PRY 0
 #
+w = open("output.txt", "a")
+w.write('Pressure, Temp, Humidity, Time')
+w.write("\n")
+w.close()
+        
 cycle = int(input("Number of cycles: "))
 for x in range (0,cycle,1):
 #
