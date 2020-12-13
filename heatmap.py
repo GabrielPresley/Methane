1ff9ee80fa35068824afafa26474fc90ffc4e669
Ethanium314 <75822728+Ethanium314@users.noreply.github.com>
2020-12-12 21:02:44 -0800

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -8,8 +8,8 @@ import random
 # Initialize plots
 fig1 = plt.figure()
 fig2 = plt.figure()
-ax1 = fig1.add_subplot(111, projection='3d')
-ax2 = fig2.add_subplot(111, projection='3d')
+ax1 = fig1.gca(projection='3d')
+ax2 = fig2.gca(projection='3d')
 
 # Add once we have methane data
 # fig3 = plt.figure()
@@ -45,12 +45,25 @@ for i in range(len(pressure)):
 
 # Plot the data to two different plots
 ax1.scatter(x, y, altitude, c=temperature)
+ax1.set_title("Temperature Heatmap")
+ax1.set_xlabel("x (m)")
+ax1.set_ylabel("y (m)")
+ax1.set_zlabel("altitude (m)")
 ax2.scatter(x, y, altitude, c=humidity)
+ax2.set_title("Humidity Heatmap")
+ax2.set_xlabel("x (m)")
+ax2.set_ylabel("y (m)")
+ax2.set_zlabel("altitude (m)")
 #ax3.scatter(x, y, altitude, c=methane)
+#ax3.set_title("Methane Heatmap")
+#ax3.set_xlabel("x (m)")
+#ax3.set_ylabel("y (m)")
+#ax3.set_zlabel("altitude (m)")
+#ax3.text2D(0.05, 0.07, "Colour = Methane")
 #plt.show()
 
 # Rotate the plot to look nicer
-ax1.view_init(30, 135)
-ax2.view_init(30, 135)
+ax1.view_init(35, 190)
+ax2.view_init(35, 190)
 #ax3.view_init(30, 135)
 plt.draw()
