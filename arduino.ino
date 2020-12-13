c709c519bc9ef76c22a9494adbbe6bc9deedb448
Earth13wells <gabrielw.presley@protonmail.com>
2020-12-12 22:42:52 -0800

    Formatting

--- arduino.ino
+++ arduino.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_GPS.h>
 const int VOL_PIN = A0;
-
+//
 void setup(){
     Adafruit_GPS GPS(&mySerial);
     Serial.begin( 9600 );
@@ -9,7 +9,7 @@ void setup(){
     GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // GPS output format
     GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // one cycle per second
 }
-
+//
 void loop(){
   //Start GPS stuff
   GPS.parse(GPS.lastNMEA()); // This splits the last GPS data read into parts
@@ -27,16 +27,15 @@ void loop(){
   // Start Methane stuff
     int value;
     float volt;
-
+//
     value = analogRead( VOL_PIN );
-
+//
     volt = value * 5.0 / 1023.0;
-
+//
     Serial.print( "Value: " );
     Serial.print( value );
     Serial.print( "  Volt: " );
     Serial.println( volt );
-
-
+//
     delay( 500 );
 }
