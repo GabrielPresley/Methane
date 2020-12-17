2820eaa0bfd024a926ac86c7e55bc92fbf8d2117
Earth13wells <gabrielw.presley@protonmail.com>
2020-12-16 18:27:36 -0800

    wip

--- windowsheatmap.py
+++ windowsheatmap.py
@@ -1,40 +1,15 @@
-import numpy as np
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import PySimpleGUI as sg
-import matplotlib
 
-fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('WINDOWS?')],
+            [sg.Button('No Thank you!'), sg.Button('Cancel')] ]
 
-matplotlib.use("TkAgg")
-
-def draw_figure(canvas, figure):
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
-    return figure_canvas_agg
-
-# Define the window layout
-layout = [
-    [sg.Text("Plot test")],
-    [sg.Canvas(key="-CANVAS-")],
-    [sg.Button("Ok")],
-]
-
-# Create the form and show it without the plot
-window = sg.Window(
-    "Matplotlib Single Graph",
-    layout,
-    location=(0, 0),
-    finalize=True,
-    element_justification="center",
-    font="Helvetica 18",
-)
-
-# Add the plot to the window
-draw_figure(window["-CANVAS-"].TKCanvas, fig)
-
-event, values = window.read()
-
+# Create the Window
+window = sg.Window('Windows', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
 window.close()
