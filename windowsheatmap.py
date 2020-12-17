696e1a8363bfe5549ee3a247e87b8d437e43d9cd
Earth13wells <gabrielw.presley@protonmail.com>
2020-12-16 18:02:09 -0800

    done

--- windowsheatmap.py
+++ windowsheatmap.py
@@ -1,5 +1,8 @@
-# hello_world.py
+# THIS IS THE WINDOWS PORT OF heatmap.py
 
 import PySimpleGUI as sg
 
 sg.Window(title="WindowsBadMkay", layout=[[]], margins=(100, 50)).read()
+image_viewer_column = [
+    [sg.Image(key="windowsport.png")],
+]
