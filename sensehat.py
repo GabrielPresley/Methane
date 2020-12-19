9309e6bba6d6b16018394dbe66164de118820b52
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-18 17:37:26 -0800

    Update sensehat.py
    added some tabs

--- sensehat.py
+++ sensehat.py
@@ -50,7 +50,7 @@ for x in range (0,cycle,1):
             t = strftime("%H:%M:%S")
             camera.capture('/home/pi/images/image_%s_%s.jpg' % (t, x))
             if (x % 100 == 0):
-            for a in range (1,3):
-                copyfile('output.txt' '/path/to/usb%s/output_%s.csv' % (a, a))
-                #Also need to do images to at least on drive.
+                for a in range (1,3):
+                    copyfile('output.txt' '/path/to/usb%s/output_%s.csv' % (a, a))
+                    #Also need to do images to at least one drive.
 #
