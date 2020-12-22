7ab75be582eb2b993ef7dda9a86b1d751532c5c6
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-21 19:18:10 -0800

    Update sensehat.py
    defined pi camera class for camera

--- sensehat.py
+++ sensehat.py
@@ -7,7 +7,7 @@
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"""
 
 import serial
-from picamera import PiCamera as camera
+from picamera import PiCamera
 from sense_hat import SenseHat
 from shutil import copyfile
 from threading import Thread
@@ -26,7 +26,9 @@ def ReadArduino():
                 wait = False
                 return line
 #
-camera.start_preview()
+#camera.start_preview()
+
+camera = PiCamera()
 
 camera.resolution = (100, 100)
 #
@@ -67,4 +69,4 @@ for x in range (0,cycle,1):
                     #copyfile('output.txt' '/path/to/usb%s/output_%s.csv' % (a, a))
                     #Also need to do images to at least one drive.
 #
-camera.stop_preview()
+#camera.stop_preview()
