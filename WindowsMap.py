f842914401eaf327fcbf65c5d68a1835237b1f72
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-22 20:55:09 -0800

    Update WindowsMap.py
    oops

--- WindowsMap.py
+++ WindowsMap.py
@@ -6,7 +6,7 @@ selection1 = tkinter.StringVar(main)
 selection1.set("thing")
 
 dropdown = []
-int i in range(4):
+for i in range(4):
     dropdown.append(tkinter.OptionMenu(main, selection1, "thing", "stuff", "doohicker"))
     dropdown[i].pack()
 
