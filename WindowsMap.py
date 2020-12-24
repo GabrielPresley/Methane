3800a6f883ad7a0bfab5666fb64a3a12c860f9bd
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-24 14:42:16 -0800

    Update WindowsMap.py
    updated the options for the dropdown menu and added a map for the options

--- WindowsMap.py
+++ WindowsMap.py
@@ -6,6 +6,8 @@ def updateGraph(event):
 
 main = tkinter.Tk()
 
+datamap = {"Pressure": 0, "Tempature": 1, "Humidity": 2, "Time": 3}
+
 data = [[1, 2, 3, 4, 5],
         [1, 3, 2, 5, 4],
         [5, 4, 3, 2, 1], 
@@ -17,7 +19,7 @@ for i in range(4):
     selection.append(tkinter.StringVar(main))
     selection[i].set("thing")
     
-    dropdown.append(tkinter.OptionMenu(main, selection[i], "thing", "stuff", "doohicker", command = updateGraph))
+    dropdown.append(tkinter.OptionMenu(main, selection[i], "Pressure", "Tempature", "Humidity", "Time", command = updateGraph))
     dropdown[i].pack()
 
 main.mainloop()
