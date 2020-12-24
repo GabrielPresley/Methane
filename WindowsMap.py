6c2fc4307d4dd69a60ed997f0632bd3a9319b773
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2020-12-23 21:29:21 -0800

    Update WindowsMap.py
    added event handler for options change

--- WindowsMap.py
+++ WindowsMap.py
@@ -12,20 +12,27 @@ for i in range(4):
 
 main.mainloop()"""
 
-
 import tkinter
+
+def updateGraph(event):
+    #will update the graph when the event is called. to be edited later
+    print("works")
 
 main = tkinter.Tk()
 
-
+data = [[1, 2, 3, 4, 5],
+        [1, 3, 2, 5, 4],
+        [5, 4, 3, 2, 1], 
+        [3, 4, 5, 1, 2]]
 
 dropdown = []
 selection = []
 for i in range(4):
-	selection.append(tkinter.StringVar(main))
-	selection[i].set("thing")
-	
-	dropdown.append(tkinter.OptionMenu(main, selection[i], "thing", "stuff", "doohicker"))
-	dropdown[i].pack()
+    selection.append(tkinter.StringVar(main))
+    selection[i].set("thing")
+    
+    dropdown.append(tkinter.OptionMenu(main, selection[i], "thing", "stuff", "doohicker"))
+    dropdown[i].bind('<Button-1>', updateGraph)
+    dropdown[i].pack()
 
 main.mainloop()
