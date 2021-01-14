c999f1bd22aeccafac950ab2a9c6ae63554ffd61
Earth13wells <gabrielw.presley@protonmail.com>
2021-01-13 19:12:14 -0800

    methane sensor

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,121 +1,104 @@
-
-// I2C SRF10 or SRF08 Devantech Ultrasonic Ranger Finder
-// by Nicholas Zambetti <http://www.zambetti.com>
-// and James Tichenor <http://www.jamestichenor.net>
-
-// Demonstrates use of the Wire library reading data from the
-// Devantech Utrasonic Rangers SFR08 and SFR10
-
-// Created 29 April 2006
-
-// This example code is in the public domain.
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+|*    This code was written by     |
+|*               MMM               |
+|*  MMM Monitors Methane was made  |
+|*   for grad 2021 BSS SRM Class   |
+|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 #include <Wire.h>
 
-void setup() {
-
-  Wire.begin();                // join i2c bus (address optional for master)
-
-  Serial.begin(9600);          // start serial communication at 9600bps
-}
-
-int reading = 0;
-
-void loop() {
-
-  // step 1: instruct sensor to read echoes
-
-  Wire.beginTransmission(112); // transmit to device #112 (0x70)
-
-  // the address specified in the datasheet is 224 (0xE0)
-
-  // but i2c adressing uses the high 7 bits so it's 112
-
-  Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)
-
-  Wire.write(byte(0x50));      // command sensor to measure in "inches" (0x50)
-
-  // use 0x51 for centimeters
-
-  // use 0x52 for ping microseconds
-
-  Wire.endTransmission();      // stop transmitting
-
-  // step 2: wait for readings to happen
-
-  delay(70);                   // datasheet suggests at least 65 milliseconds
-
-  // step 3: instruct sensor to return a particular echo reading
-
-  Wire.beginTransmission(112); // transmit to device #112
-
-  Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
-
-  Wire.endTransmission();      // stop transmitting
-
-  // step 4: request reading from sensor
-
-  Wire.requestFrom(112, 2);    // request 2 bytes from slave device #112
-
-  // step 5: receive reading from sensor
-
-  if (2 <= Wire.available()) { // if two bytes were received
-
-    reading = Wire.read();  // receive high byte (overwrites previous reading)
-
-    reading = reading << 8;    // shift high byte to be high 8 bits
-
-    reading |= Wire.read(); // receive low byte as lower 8 bits
-
-    Serial.println(reading);   // print the reading
-
-  }
-
-  delay(250);                  // wait a bit since people have to read the output :)
-}
-
-/*
-
-// The following code changes the address of a Devantech Ultrasonic Range Finder (SRF10 or SRF08)
-
-// usage: changeAddress(0x70, 0xE6);
-
-void changeAddress(byte oldAddress, byte newAddress)
-
+// address is 0x50
+// This is according to the example code in the
+// wire library i2c_scanner
+#define Addr 0x50
+//
+void setup()
 {
+  // open I2C comm. as master
+  Wire.begin();
+  // set baud rate = 9600
+  Serial.begin(9600);
+}
+//
+void loop()
+{
+  // start I2C
+  Wire.beginTransmission(Addr);
+//
+  // data register/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+|*    This code was written by     |
+|*               MMM               |
+|*  MMM Monitors Methane was made  |
+|*   for grad 2021 BSS SRM Class   |
+|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-  Wire.beginTransmission(oldAddress);
+#include <Wire.h>
 
-  Wire.write(byte(0x00));
-
-  Wire.write(byte(0xA0));
-
+// address is 0x50
+// This is according to the example code in the
+// wire library i2c_scanner
+#define Addr 0x50
+//
+void setup()
+{
+  // open I2C comm. as master
+  Wire.begin();
+  // set baud rate = 9600
+  Serial.begin(9600);
+}
+//
+void loop()
+{
+  // start I2C
+  Wire.beginTransmission(Addr);
+//
+  // data register
+  Wire.write(0x00);
+//
+  // stop I2C transmission
   Wire.endTransmission();
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(byte(0xAA));
-
-  Wire.endTransmission();
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(byte(0xA5));
-
-  Wire.endTransmission();
-
-  Wire.beginTransmission(oldAddress);
-
-  Wire.write(byte(0x00));
-
-  Wire.write(newAddress);
-
-  Wire.endTransmission();
-
+//
+  // request 2 bytes
+  Wire.requestFrom(Addr, 2);
+//
+  // read 2 bytes
+  unsigned int data[2];
+  if(Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+//
+  // make 12-bits
+  int raw_adc = ((data[0] & 0x0F) * 256) + data[1];
+  float ppm = (10000.0 / 4096.0) * raw_adc + 200.0;
+// output
+  Serial.print("Methane (ppm)");
+  Serial.println(ppm);
+  delay(1000);
 }
 
-*/
+  Wire.write(0x00);
+//
+  // stop I2C transmission
+  Wire.endTransmission();
+//
+  // request 2 bytes
+  Wire.requestFrom(Addr, 2);
+//
+  // read 2 bytes
+  unsigned int data[2];
+  if(Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+//
+  // make 12-bits
+  int raw_adc = ((data[0] & 0x0F) * 256) + data[1];
+  float ppm = (10000.0 / 4096.0) * raw_adc + 200.0;
+// output
+  Serial.print("Methane (ppm)");
+  Serial.println(ppm);
+  delay(1000);
+}
