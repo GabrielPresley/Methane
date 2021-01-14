d058b943f0b916aa96864d4644149d211d2e7d9b
Earth13wells <gabrielw.presley@protonmail.com>
2021-01-14 10:45:03 -0800

    raspberry.py

--- raspberry.py
+++ raspberry.py
@@ -9,17 +9,28 @@
 #import modules
 import serial
 from shutil import copyfile
-from threading import Thread
 from time import sleep, strftime
-
+from picamera import PiCamera
+#
 # open serial port
 ser = serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.open()
-
-
-while True:
+camera = PiCamera()
+camera.resolution = (100, 100)
+w = open("output.csv", "a")
+#
+cycle = int(input("Number of cycles: "))
+for x in range (0,cycle,1):
     arduinodata = string(ser.read())
-    print(arduinodata)
-    pass
-
-ser.close()
+    w.write(arduinodata)
+    w.write("\n")
+    sleep(1)
+    if (x % 5 == 0 ):
+        #Updates time
+        t = strftime("%H:%M:%S")
+        #takes a photo and saves it with the time and number
+        camera.capture(("/home/pi/images/image_%s_%s.jpg" % (t, x)))
+    #every 100 cycles it backs up the data to a usb drive
+    if (x % 100 == 0):
+        for a in range (1,3):
+            copyfile('output.txt' '/path/to/usb%s/output_%s.csv' % (a, a))
