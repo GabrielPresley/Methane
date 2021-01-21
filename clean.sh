eafbd6be6b3c9aa371e19e103644dce4cbed7663
Earth13wells <gabrielw.presley@protonmail.com>
2021-01-20 21:37:48 -0800

    Data formatting tool

--- clean.sh
+++ clean.sh
@@ -2,7 +2,6 @@
 |*    This code was written by     |
 |*               MMM               |
 |*  MMM Monitors Methane was made  |
-|*   for grad 2021 BSS SRM Class   |
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ '
 echo "" > $2
  grep -oP '(?<=Methane\ \(PPM\)).*' $1 >> $2
@@ -10,4 +9,7 @@ echo "" > $2
  grep -oP '(?<=Temperature\ \(\*c\)).*' $1 >> $2
  grep -oP '(?<=Pressure\ \(hPa\)).*' $1 >> $2
  grep -oP '(?<=Altitude\ \(m\)).*' $1 >> $2
- grep ^49 $1 >> $2
+ awk '/^49/' $1 >> $2
+ sed -i 's/$/, /' $2
+
+cat $2 | datamash transpose --field-separator=, > $3
