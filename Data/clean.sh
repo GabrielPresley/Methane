9f74d6b00b89f2a7c6ca6cb4536c8f9f073b8bcd
Earth13wells <gabrielw.presley@protonmail.com>
2021-02-11 11:11:27 -0800

    now works with the beeees

--- Data/clean.sh
+++ Data/clean.sh
@@ -30,22 +30,23 @@
 touch /tmp/step0
 touch /tmp/step1
 touch /tmp/step2
-awk '{n=$2}l1!=n{if(p)print l0; print; p=0}l1==n{p=1}{l0=$0; l1=n}END{print}' $1 > /tmp/step1
+#awk '{n=$2}l1!=n{if(p)print l0; print; p=0}l1==n{p=1}{l0=$0; l1=n}END{print}' $1 > /tmp/step1
 #
+cat $1 > /tmp/step1
 echo "! -CH ! " > /tmp/step2
-grep -oP '(?<=\+).*' /tmp/step1 >> /tmp/step2
+grep -oP '(?<=\+).*\b' /tmp/step1 >> /tmp/step2
 #
 echo "! -HMD ! " >> /tmp/step2
-grep -oP '(?<=^&).*' /tmp/step1 >> /tmp/step2
+grep -oP '(?<=^&).*\b' /tmp/step1 >> /tmp/step2
 #
 echo "! -°C ! " >> /tmp/step2
-grep -oP '(?<=^@).*' /tmp/step1 >> /tmp/step2
+grep -oP '(?<=^@).*\b' /tmp/step1 >> /tmp/step2
 #
 echo "! -hPA ! " >> /tmp/step2
-grep -oP '(?<=^#).*' /tmp/step1 >> /tmp/step2
+grep -oP '(?<=^#).*\b' /tmp/step1 >> /tmp/step2
 #
 echo "! -M-UP ! " >> /tmp/step2
-grep -oP '(?<=^%).*' /tmp/step1 >> /tmp/step2
+grep -oP '(?<=^%).*\b' /tmp/step1 >> /tmp/step2
 #
 echo "! -LAT ! " >> /tmp/step2
 grep -oP '(?<=,A,).*(?=,N,)' /tmp/step1 >> /tmp/step2
@@ -54,31 +55,32 @@ echo "! -LON ! " >> /tmp/step2
 grep -oP '(?<=,N,).*(?=,W,)' /tmp/step1 >> /tmp/step2
 #
 echo "! -SEC ! " >> /tmp/step2
-grep -oP '(?<=^:).*' /tmp/step1 >> /tmp/step2
+grep -oP '(?<=^\:).*\b' /tmp/step1 >> /tmp/step2
 #
-
-sed -i 's/\(^.\{1,10\}\).*/\1/' /tmp/step2
+sed -i 's/\(^.\{1,10\}\).*/\1/' /tmp/step2 #Trim lines to x length
 #
 cat /tmp/step2 | datamash transpose --field-separator=, > $2
-sed -i 's/^.,//g' $2
-sed -i 's/! /\n/g' $2
-sed -i 's/^. ,//g' $2
-sed -i 's/\\r//g' $2
 #
-sed -i 's/.$//; s/^.//' $2
+sed -i 's/^.,//g' $2 #Remove leading commas
+sed -i 's/! /\n/g' $2 # replacing "! " "\n" for titles
+sed -i 's/^. ,//g' $2 #Remove leading "x ,"
+sed -i 's/\\r//g' $2 # reomve /r from end of lines
+#
+
+sed -i 's/.$//; s/^.//' $2 #Remove first/last charecters
 #
 rm /tmp/step1
 rm /tmp/step2
 
 sed -i '2~2d' $2 #Remove Lables
 sed -i '1d' $2 # Remove Empty First Line
-
-length=$(($(head -n 7 $2 | tail -n 1 | sed 's/[^,]//g' | wc -c)-$3 ))
+#
+length=$(($(head -n 7 $2 | tail -n 1 | sed 's/[^,]//g' | wc -c)-$3 )) # Counts data point in line 7
 echo $length
-for i in 1 2 3 4 5 6 7; do
+for i in 1 2 3 4 5 6 7 8; do
 head -n $i $2 | tail -n 1 | cut -d, -f1-$length >> /tmp/step0
 done
-
+#
 cat /tmp/step0 > $2
 rm /tmp/step0
 echo "DONE"
