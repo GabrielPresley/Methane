52216948e2150753fa32440b7cac9cb0e0a8c0fd
Earth13wells <gabrielw.presley@protonmail.com>
2021-02-12 21:15:15 -0800

    "fixed" Deprecation Warnings

--- heatmapV2.py
+++ heatmapV2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
